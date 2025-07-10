d4dc1c4a9897cb24ca6b713ffcfb786484d9211a
Emmanuel <esiziba71@gmail.com>
2025-07-10 16:52:00 +0200

    javascript code

---
+++ tip-calculator/script.js
@@ -0,0 +1,16 @@
+const btnEI = document.getElementById("calculate");
+const billInput = document.getElementById("bill");
+const tipInput = document.getElementById("tip");
+const totalSpan = document.getElementById("total");
+
+function calculate(){
+const billValue = billInput.value;
+const tipValue = tipInput.value;
+
+const totalValue = billValue * (1 + tipValue / 100);
+totalSpan.innerText = totalValue.toFixed(2);
+
+}
+
+
+btnEI.addEventListener("click", calculateTotal);
